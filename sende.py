e1b4bf38e6a844107368819d52df951ddff35b1b
Oriku Precious <bamideleprecious85@gmail.com>
2024-07-25 18:50:02 -0700

    Update sende.py

--- sende.py
+++ sende.py
@@ -101,7 +101,7 @@ def review_email(email,fullName):
                 <p>Thanks for choosing Cosmetic Creations Skin Sanctuary & Med Spa. I hope you enjoyed your visit to Cosmetic Creations.</p>
                 <p>Please consider leaving a review on Google and/or Yelp as it really helps us. Please mention your Aesthetician's name when leaving a review. If you have already left us a review then we thank you! ❤️ Cosmetic Creations Team!</p>
                 <div class="button-container">
-                    <a href="https://cosmetic-chatbot.onrender.com/gogn" class="button">Review Us</a>
+                    <a href="https://cosmetic-chatbot-ody8.onrender.com/gogn" class="button">Review Us</a>
                 </div>
                 <p>Sincerely,</p>
                 <p>Rebecca Rotter</p>
